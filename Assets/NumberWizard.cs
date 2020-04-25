83db41272568f36b166bc6255e06ea731f261078
Mikołaj Nawrocki <m.nawrocki793@gmail.ocm>
2020-04-25 13:16:58 +0200

    10. Introducing Variables

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
